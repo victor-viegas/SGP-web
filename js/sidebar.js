2429e35a522e87fbaa74562358f98e3889ed4417
unknown <victorviegas036@gmail.com>
2024-02-27 15:51:03 -0300

    script do menu lateral

---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let arrow = document.querySelectorAll(".arrow"); //Retorna uma NodeList de todos os elementos descendentes do elemento
+    for(let i = 0; i < arrow.length;i++){
+        arrow[i].addEventListener("click", (e)=>{
+            let arrowParent = e.target.parentElement.parentElement; //seleciona o pai principal da seta
+            arrowParent.classList.toggle("showMenu");
+        });
+    }
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu")
+    console.log(sidebar);
+    sidebarBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("close");
+    });
+})
